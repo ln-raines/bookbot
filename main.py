ad9d4cd7ec206ea5464f6f7c8dafd8c01bcbe048
ln-raines <lydia.raines@gmail.com>
2025-08-15 17:17:43 -0400

    Added word count and character count dictionary

---
+++ main.py
@@ -0,0 +1,19 @@
+
+from stats import *
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+
+    print(f"{num_words} words found in the document")
+    print(chars_dict)
+
+
+main()
