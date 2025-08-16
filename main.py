5f9ada8d1d5f81490c4b3e35b010628624786e07
ln-raines <lydia.raines@gmail.com>
2025-08-16 12:19:56 -0400

    Added user input for books

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from stats import *
+import sys
 
 def get_book_text(path):
     with open(path) as f:
@@ -15,10 +16,15 @@ def print_report(book_path, num_words, chars_sorted_list):
         print(f"{item['char']}: {item['num']}")
     print("============= END ===============")
 
-
+def file_path():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else: 
+        return sys.argv[1]
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = file_path()
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
